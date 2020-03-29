cc7e68579b44a00a5cc1298a7716f73197f42d75
Sven Carlin <s.carlin@arcor.de>
2020-03-29 12:38:45 +0200

    API request type

--- lambda.go
+++ lambda.go
@@ -11,6 +11,21 @@ type LambdaApiResponse struct {
 	Base64  bool              `json:"isBase64Encoded"`
 }
 
+type LambdaApiRequest struct {
+	Resource   string            `json:"resource"`
+	Path       string            `json:"path"`
+	Method     string            `json:"httpMethod"`
+	Headers    map[string]string `json:"headers"`
+	PathParams map[string]string `json:"pathParameters"`
+	Body       string            `json:"body"`
+	Base64     bool              `json:"isBase64Encoded"`
+}
+
+type LambdaApiBody struct {
+	Error string      `json:"error,omitempty"`
+	state interface{} `json:"state"`
+}
+
 func StringifyBody(response *LambdaApiResponse, body interface{}) LambdaApiResponse {
 	b, _ := json.Marshal(body)
 	response.Body = string(b)
